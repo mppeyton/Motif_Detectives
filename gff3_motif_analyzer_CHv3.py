fd272762fe92598a88b8b389d1db2a22c7b73da2
Heil <cheil@PHARMPHYS15ML.local>
2022-10-23 17:13:21 -0400

    updated output format

--- gff3_motif_analyzer_CHv3.py
+++ gff3_motif_analyzer_CHv3.py
@@ -40,25 +40,29 @@ with open (file_gff,'r') as gtf:
 				new_description = split_description[0] 
 # changed line2 into string instead of list
 #		line2 = [Chromosome, feature, start, end, new_description]
-		line2 = 'Chr' + Chromosome + ' ' + start + ' : ' + end + '\t' + feature + '\t' + new_description
+		line2 = 'Chr' + Chromosome + '\t' + feature + '\t ' + start + '\t' + end + '\t' + new_description
 #		print(line2)
 #	loop over hits and feed in start and end coordinates from hit_dict
 		for hit in hit_dict:
 			p_start = hit_dict[hit][0]
 			p_end = hit_dict[hit][1]
+			line1 = motif + '\t' + p_start + '\t' + p_end
 # I compare each line to evaluate the positions, if match, then I add it to the gene_feature dict 
 			if int(p_start) >= int(start) and int(p_end) <= int(end):
 #			if int(p_start) >= int(start):
 #				if int(p_end) <= int(end):
 # With extend I add the data to be retrieved in the same dict
-# added motif to output
+# added motif and coordinates to output
 #					gene_feature.setdefault('Chromosome.' + Chromosome, []).extend([line2])
-					gene_feature.setdefault('Chromosome.' + Chromosome, []).extend(['motif ' + motif + ' found in ' + line2])
+					gene_feature.setdefault('Chromosome.' + Chromosome, []).extend([line1 + '\t' + line2])
 #print(gene_feature)
 
-for key, values in gene_feature.items():
-#	print(values)
-	for i in values:
-		print(i) 
+with open('motif_hits_mapped.out', 'w') as output:
+	output.write(f"# motif\tstart\tend\tchr\tfeature\tstart\tend\tID/description\n")
+	for key, values in gene_feature.items():
+	#	print(values)
+		for i in values:
+			output.write(i + '\n')
+#			print(i) 
 
 		
