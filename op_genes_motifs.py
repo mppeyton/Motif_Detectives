7c29d9a51a9318ef96a3b022b4cda5b6b7e2ecb0
student <student@info-14.cshl.edu>
2022-10-23 18:00:06 -0400

    gene hits in chromosomeI from single cell reference RNAseq data

---
+++ op_genes_motifs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+with open ('/Users/student/Downloads/up_genes_1cell_embryo.txt','r') as genes:
+  next(genes)
+  for line in genes:
+    line = line.rstrip()
+    gene , gene_id , fc , pval = line.split('\t')
+    line2 = gene_id
+
+    with open('motif_hits_mapped.out','r') as motif_matches:
+      next(motif_matches)
+      for line3 in motif_matches:
+        line3 = line3.rstrip()
+        motif, start , end , chromosome , feature , start , end ,description  = line3.split('\t')
+        line4 = description
+        if line2 in line4:
+          print(line2,line4)
+
