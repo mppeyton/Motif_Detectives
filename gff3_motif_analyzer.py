ac4ecaa07822e819b281e7fab6830aa462b6e423
Heil <cheil@PHARMPHYS15ML.local>
2022-10-23 20:39:25 -0400

    final gff3_motif_analyzer script

--- gff3_motif_analyzer.py
+++ gff3_motif_analyzer.py
@@ -2,30 +2,56 @@
 
 import sys
 
-file_gff = "Caenorhabditis_elegans.WBcel235.108.chromosome.I.gff3"
+# parse hits from motif_finder.py
 
-#This is the dict where I will save the matched positions
-gene_feature={}
+hit_dict = {}
 
-
-with open('Chromosome.1_hits.txt','r') as hits, open (file_gff,'r') as gtf:
+with open('motif_hit_out.txt','r') as hits:
 	for line in hits:
 		line = line.rstrip()
-		chromosome,motif,p_start,p_end = line.split()
-			for line in gtf:
-				line = line.rstrip()	
-				if line.startswith('#'):
-					continue
-				else:
-					Chromosome, Wormbase, feature, start, end, a, strand, b, description = line.split('\t')
-					line2 = [Chromosome,feature, start, end, description]
-			# I compare each line to evaluate the positions, if match, then I add it to the gene_feature dict 
-					if int(p_start) >= int(start):
-						if int(p_end) <= int(end):
-			# With extend I add the data to be retrieved in the same dict
-							gene_feature.setdefault('Chromosome.'+Chromosome, []).extend([line2])
-					for key, values in gene_feature.items():
-						for i in values:
-							print(i) 
+		if line.startswith('#'):
+			continue
+		chromosome, motif, p_start, p_end = line.split()
+		hit = chromosome + ": " + motif
+		hit_dict[hit] = [p_start, p_end]
 
-		
+# parse .gff3 file
+
+file_gff = "Caenorhabditis_elegans.WBcel235.108.chromosome.I.gff3"
+gene_feature={}
+
+with open (file_gff,'r') as gtf:
+# skip over the first 9 lines of the file
+	for line in range(8):
+		next(gtf)
+	for line in gtf:
+		line = line.rstrip()	
+		if line.startswith('#'):
+			continue
+		else:
+			Chromosome, Wormbase, feature, start, end, a, strand, b, description = line.split('\t')
+			split_description = description.split(';')
+			if feature == 'gene':
+				new_description = split_description[0] + ' ' + split_description[3]
+			else:
+				new_description = split_description[0] 
+		line2 = 'Chr' + Chromosome + '\t' + feature + '\t ' + start + '\t' + end + '\t' + new_description
+#	loop over hits in hit_dict and feed in start and end coordinates of motifs
+		for hit in hit_dict:
+			p_start = hit_dict[hit][0]
+			p_end = hit_dict[hit][1]
+			line1 = motif + '\t' + p_start + '\t' + p_end
+# find motif positions of hits in the annotated genome
+			if int(p_start) >= int(start) and int(p_end) <= int(end):
+# extend data to gene_feature dict
+				gene_feature.setdefault('Chromosome.' + Chromosome, []).extend([line1 + '\t' + line2])
+
+# create output file
+with open('motif_hits_mapped.out', 'w') as output:
+	output.write(f"# motif\tstart\tend\tchr\tfeature\tstart\tend\tID/description\n")
+	for chromosome, entries in gene_feature.items():
+		for mapped_hit in entries:
+			output.write(mapped_hit + '\n')
+#	print to gui
+			print(f"# motif\tstart\tend\tchr\tfeature\tstart\tend\tID/descripti    on")
+			print(mapped_hit)
