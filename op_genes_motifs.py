2bba6f0514c9a5e20fa810bfc50d4e14002083cb
Marleny Garcia Lozano <marlenygar01@gmail.com>
2022-10-24 11:10:24 -0400

    updating script with new input file name

---
+++ op_genes_motifs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+with open ('up_genes_1cell_embryo.txt','r') as genes:
+  next(genes)
+  for line in genes:
+    line = line.rstrip()
+    gene , gene_id , fc , pval = line.split('\t')
+    line2 = gene_id
+
+    with open('mapped_motif_hits.out','r') as motif_matches:
+      next(motif_matches)
+      for line3 in motif_matches:
+        line3 = line3.rstrip()
+        motif, start , end , chromosome , feature , start , end ,description  = line3.split('\t')
+        line4 = description
+        motif1=motif
+        if line2 in line4:
+          print('upregulated gene: '+line2+' contains this motif:' +motif1,'annotation',line4)
+
