fd5118fd44e02b68901908bb4384bbdc5d921dd8
mppeyton <mina.peyton@gmail.com>
2022-10-24 11:37:32 -0400

    comments updated

--- md_gui.py
+++ md_gui.py
@@ -4,7 +4,7 @@
 # GUI and fasta motif extraction By: Mina Peyton
 # gff motif extraction By: Chrissy Heil 
 # CSHL Programming for Biology - Final Project: Motif Detective GUI
-# Input: Species & Motif Sequence
+# Input: theorectically would take the organism name & motif sequence 
 # Output: fasta and gff3 extraction of motif sequence positions & annotations
 
 import gzip
