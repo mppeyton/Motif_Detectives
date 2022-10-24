60948eb2c3e34c7ce22c1d31fbc55e6d44f8273e
Cynthia SC <49501255+cyntsc@users.noreply.github.com>
2022-10-24 11:08:01 -0500

    Update md_fasta_parser.py

--- md_fasta_parser.py
+++ md_fasta_parser.py
@@ -1,42 +1,36 @@
-#!/usr/bin/env python3
-import gzip
+#!/usr/bin/env python3 
+
+# 22Oct2022 
+# By: Aparna Mariyam Thomas, Chrissy Heil, & Mina Peyton
+# CSHL Programming for Biolgy - Final Project: Motif Detective
+# Input: fasta files from a directory
+# Output: dictionary {Chromosome_number: sequence}
+
 import glob
 import re
-import sys
+ 
 cgenome = {}
-  
-def open_fasta(fasta_file):
-  for file_name in glob.glob(fasta_file):
-    seqs=[]
-    with gzip.open(fasta_file,'rt') as raw:
-      label_regex = re.compile(r'([IVX]+)')
-      label = str(label_regex.findall(file_name)).strip('[]')		
-      label = label.replace("'","")
-      chromosome_label = 'Chromosome.'+label
-      for line in raw:	
-        if line.startswith('>'):
-          next
-        else:
-          line = line.rstrip()
-          seqs.append(line)
-      sequence = ''.join(seqs)
-      cgenome[chromosome_label]=sequence
-  return cgenome
+ 
+for file_name in glob.glob('C*.fa'):
+	seq = ''
+	with open(file_name,'r') as raw:
+#		print(name)
+		label_regex = re.compile(r'([IVX]+)')
+		label = str(label_regex.findall(file_name)).strip('[]')
+		label = label.replace("'","")
+#		print(label)
+		chromosome_label = 'Chromosome.'+label
+#   	print(chromosome_label)
+		for line in raw:
+			if line.startswith('>'):
+#				print(line)
+				next
+			else:
+				line = line.rstrip()
+				seq += line
+	cgenome[chromosome_label]=seq
 
-def main():
-  progname=sys.argv[0]
-  usage = "\n\n\tusage: {} fasta_file\n\n\n".format(progname)
+print(cgenome)
 
-  if len(sys.argv) < 1 :
-    sys.stderr.write(usage)
-    sys.exit(1)
-
-#capture command-line arguments 
-  fasta_file=sys.argv[1]
-  parsed_genome=open_fasta(fasta_file)
-  print(parsed_genome)
-  sys.exit(0)
-
-if __name__ == "__main__":
-    main()
-
+#for i in cgenome.keys():
+#	print(i,cgenome[i][0:3])
